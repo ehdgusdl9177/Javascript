c9d5e003fec91129787286d4ca288a3f474c477d
ehdgusdl9177 <k23129177@gmail.com>
2022-06-30 17:19:09 +0900

    add function fix

--- Section2/assets/scripts/app.js
+++ Section2/assets/scripts/app.js
@@ -1,15 +1,10 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  const calcDescription = `${currentResult} + ${parseInt(userInput.value)}`;
+  currentResult = currentResult + parseInt(userInput.value);
+  outputResult(currentResult, calcDescription);
 }
 
-const additionResult = add();
-
-currentResult = add(1, 2);
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener("click", add);
