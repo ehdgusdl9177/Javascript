8bad9aab0bb1e7bfad3631579276c90d40ef8e5c
ehdgusdl9177 <k23129177@gmail.com>
2022-07-27 10:44:52 +0900

    Data Type

--- Section2/assets/scripts/app.js
+++ Section2/assets/scripts/app.js
@@ -1,13 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// Gets input from input field
 function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
+// 계산 로그 생성과 작성
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-  outputResult(currentResult, calcDescription);
+  outputResult(currentResult, calcDescription); // from vendor file
 }
 
 function add() {
